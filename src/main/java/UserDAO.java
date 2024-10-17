09f74e7bb8ffe7823aed277f1d5f23d2f3bc3340
thinh2711 <thinhcuc6@gmail.com>
2024-10-17 22:57:46 +0700

    Api multithreading

--- src/main/java/UserDAO.java
+++ src/main/java/UserDAO.java
@@ -17,37 +17,37 @@ import java.sql.SQLException;
 
 import java.io.IOException;
 import java.util.Objects;
-public class UserDAO {
-    public boolean register(String username, String password) {
-        String sql = "INSERT INTO users (user_name, password, is_admin) VALUES (?, ?, false)";
-
-        try (Connection conn = connect();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setString(1, username);
-            pstmt.setString(2, password);
-
-            int rowsAffected = pstmt.executeUpdate();// true nếu đăng ký thành công
-
-            return rowsAffected > 0;
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return false;
-    }
-    public static Connection connect() {
-        Connection conn = null;
-        try {
-            String url = "jdbc:sqlite:data/liba.db";
-            // Kết nối tới cơ sở dữ liệu
-            conn = DriverManager.getConnection(url);
-            System.out.println("Kết nối thành công đến cơ sở dữ liệu!");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
-}
+//public class UserDAO {
+//    public boolean register(String username, String password) {
+//        String sql = "INSERT INTO users (user_name, password, is_admin) VALUES (?, ?, false)";
+//
+//        try (Connection conn = connect();
+//             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+//
+//            pstmt.setString(1, username);
+//            pstmt.setString(2, password);
+//
+//            int rowsAffected = pstmt.executeUpdate();// true nếu đăng ký thành công
+//
+//            return rowsAffected > 0;
+//
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//
+//        return false;
+//    }
+//    public static Connection connect() {
+//        Connection conn = null;
+//        try {
+//            String url = "jdbc:sqlite:data/liba.db";
+//            // Kết nối tới cơ sở dữ liệu
+//            conn = DriverManager.getConnection(url);
+//            System.out.println("Kết nối thành công đến cơ sở dữ liệu!");
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return conn;
+//    }
+//}
 // tính năng đăng ký dùng dòng lệnh
--- src/main/java/org/example/menubar/APIIntegration.java
+++ src/main/java/org/example/menubar/APIIntegration.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
